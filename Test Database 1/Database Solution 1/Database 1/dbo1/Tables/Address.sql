488d1c4a566b659c832a2b175e9b9bf7532d20fa
Omar <omarzahir12@gmail.com>
2021-06-10 01:34:23 -0400

    merged and published inital data

---
+++ Test Database 1/Database Solution 1/Database 1/dbo1/Tables/Address.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Address]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [PersonId] INT NULL, 
+    [StreetAddress] NVARCHAR(50) NULL, 
+    [City] NVARCHAR(50) NULL, 
+    [Province] NVARCHAR(50) NULL, 
+    [PostalCode] NVARCHAR(50) NULL
+    CONSTRAINT [FK_Address_Person] FOREIGN KEY ([PersonId]) REFERENCES [Person]([Id])
+
+)
